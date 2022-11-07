935846c7375d610ed05388afe580a61ba09e3aa3
Joji Augustine <jojimail@gmail.com>
2022-11-07 12:49:28 +0100

    chore: test

---
+++ repo2/packages/app2/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Add comment 13
+app.get("/", (req, res) => {
+  res.send("Hello from Repo1!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
